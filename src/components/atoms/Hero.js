73ec9d867e4bdd0ed829f21602ec699fc97a611e
AntonioCarlos17 <acarlos401@gmail.com>
2023-12-30 10:51:31 -0300

    colocação da ferramentas para desenvolvimento

---
+++ src/components/atoms/Hero.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+  
+const Hero = () => (
+    <>
+        <h1>Sabor da Casa</h1>
+    </>
+);
+ 
+ 
+export default Hero;
---
+++ src/components/atoms/Hero.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Hero from "./Hero";
+ 
+export default {
+  title: "Components/atoms/Hero",
+  component: Hero
+};
+ 
+export const Usage = () => (
+<Hero />
+);
